2b6afd9fdda1a7996aba455bddba6a6ab932aca5
zguiping <907070621@qq.com>
2023-03-15 15:19:16 +0800

    首页修改

--- static/web/js/fuxi.js
+++ static/web/js/fuxi.js
@@ -5,45 +5,49 @@ $(document).ready(function(){
 
     // 指定图表的配置项和数据--条形图
     option = {
-        title: {
-            text: '截止2023年2月21日&nbsp党员总数为27人',
-            left:'2%',
-            top:'8%',
-            textStyle: {
-                fontSize: 12,
-                color:'#FFFFFF'
-            }
-        },
         xAxis: {
             type: 'value',
             boundaryGap: [0, 0.01],
             axisTick: false,
-            show: true,
             splitLine:{
-                show:true
+                show:true,
+                lineStyle:{ 
+                    type: "solid", //实线
+                    shadowColor: "#B9BFC8",  // 透明度颜色
+                    opacity: 0.1, //透明度
+                    width: 1, 
+                    type: 'solid' 
+                }
             },
             textStyle:{
                 fontSize:'12'
             },
             offset:2,
             axisLine:{
+                show: true,
                 lineStyle: {
                     color: "rgba(255, 251, 248, 1)"
+
                 }
             }
         },
         yAxis: {
             type: 'category',
             data: ["小于5年","5年至10年","10年至20年","20年以上"],
-            show: true,
             axisTick: false,
-            axisLine: {
+            offset:2,
+            axisLabel: {
+                show: true, //显示坐标轴上的文字\
+            },
+            splitLine:{
+                show:false,
+            },
+            axisLine:{
                 show: false,
                 lineStyle: {
                     color: "rgba(255, 251, 248, 1)"
                 }
             },
-            offset:2,
             nameTextStyle: {
                 color:'#FFFFFF'
             }
@@ -52,28 +56,20 @@ $(document).ready(function(){
             {
                 name: '2012',
                 type: 'bar',
+                colorBy: 'data',
                 barWidth:'30%',
                 data: [10, 8, 19, 38],
                 itemStyle: {
                     normal: {
                         label: {
-                          show: true,
+                        show: true,
                           position: 'right', //数值在上方显示
                         }
                     }
                 }
             }
         ],
-        // z柱状图颜色设置
-        itemStyle:{
-            normal:{
-            barBorderRadius: 5,
-                color:function(params){
-                    var colorList=['#7BDF82','#36D8D8','#FF862D', '#FF2F20'];
-                    return colorList[params.dataIndex];
-                }
-            }
-        },
+        color: ['#7BDF82','#36D8D8','#FF862D', '#FF2F20'],
         grid:{
             show:false,
             top:'25%',
@@ -85,5 +81,185 @@ $(document).ready(function(){
 
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
+
+
+    // 三会一课环形图
+    var myChartSh1 = echarts.init(document.getElementById('sh-bz1'));
+    var myChartSh2 = echarts.init(document.getElementById('sh-bz2'));
+    var myChartSh3 = echarts.init(document.getElementById('sh-bz3'));
+    var myChartSh4 = echarts.init(document.getElementById('sh-bz4'));
+    var myChartSh5 = echarts.init(document.getElementById('educationChart'));
+
+    var showText = '';
+    var shOption1 = {
+        color: ['#36D8D8' , '#44546B'],
+        series: [{	
+            type: 'pie',
+            radius: ['60%', '85%'],
+            label: {
+                show: true,
+                position: 'center',
+                fontStyle: 'normal',
+                fontSize: 16,
+                color: '#fff',
+                formatter: (params) => {
+                    if(params.dataIndex == 0) {
+                        showText =  Math.round(params.percent) + '%';
+                    }
+                    return showText;
+                },
+            },
+            data: [{
+                value: 6,
+            }, {
+                value: 22 - 6
+            }],
+            emphasis: {
+                disabled: true,
+            }
+        }]
+    };
+
+    var shOption2 = {
+        color: ['#FF2F20' , '#44546B'],
+        series: [{	
+            type: 'pie',
+            radius: ['60%', '85%'],
+            label: {
+                show: true,
+                position: 'center',
+                fontStyle: 'normal',
+                fontSize: 16,
+                color: '#fff',
+                formatter: (params) => {
+                    if(params.dataIndex == 0) {
+                        showText =  Math.round(params.percent) + '%';
+                    }
+                    return showText;
+                },
+            },
+            data: [{
+                value: 13,
+            }, {
+                value: 22 - 13
+            }],
+            emphasis: {
+                disabled: true,
+            }
+        }]
+    };
+
+
+    var shOption3 = {
+        color: ['#7BDF82' , '#44546B'],
+        series: [{	
+            type: 'pie',
+            radius: ['60%', '85%'],
+            label: {
+                show: true,
+                position: 'center',
+                fontStyle: 'normal',
+                fontSize: 16,
+                color: '#fff',
+                formatter: (params) => {
+                    if(params.dataIndex == 0) {
+                        showText = Math.round(params.percent) + '%';
+                    }
+                    return showText;
+                },
+            },
+            data: [{
+                value: 0,
+            }, {
+                value: 22 - 0
+            }],
+            emphasis: {
+                disabled: true,
+            }
+        }]
+    };
+
+    var shOption4 = {
+        color: ['#FF862D' , '#44546B'],
+        series: [{	
+            type: 'pie',
+            radius: ['60%', '85%'],
+            label: {
+                show: true,
+                position: 'center',
+                fontStyle: 'normal',
+                fontSize: 16,
+                color: '#fff',
+                formatter: (params) => {
+                    if(params.dataIndex == 0) {
+                        showText =  Math.round(params.percent) + '%';
+                    }
+                    return showText;
+                },
+            },
+            data: [{
+                value: 3,
+            }, {
+                value: 22 - 3
+            }],
+            emphasis: {
+                disabled: true,
+            }
+        }]
+    };
+
+    // 学历分布环形图
+    var shOption5 = {
+        legend: { // 图例设置
+            orient: "vertical",
+            right: '25%',
+            top: 'middle',
+            textStyle: {
+                color: '#fff'
+            }
+        },
+        color: ['#36D8D8' , '#975FE5', '#FF2F20', '#7BDF82'],
+        series: [{	
+            type: 'pie',
+            right: '55%',
+            radius: ['60%', '85%'],
+            label: {
+                show: true,
+                position: 'center',
+                fontStyle: 'normal',
+                fontSize: 16,
+                color: '#fff',
+                formatter: '总数（人）\n 75',
+                lineHeight: 26
+            },
+            data: [{
+                name:"本科以上：1人",
+                value: 1,
+            }, {
+                name: "本科：3人",
+                value: 3
+            }, {
+                name: "大专：20人",
+                value: 20
+            }, {
+                name: "大专以下：51人",
+                value: 51
+            }],
+            emphasis: {
+                disabled: true,
+            }
+        }]
+    };
+    
+    myChartSh1.setOption(shOption1);
+    myChartSh2.setOption(shOption2);
+    myChartSh3.setOption(shOption3);
+    myChartSh4.setOption(shOption4);
+    myChartSh5.setOption(shOption5);
+
+    // window.addEventListener("resize",function(){
+    //     myChartSh1.resize();
+    // });
+    
 
 });
